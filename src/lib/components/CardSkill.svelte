3e7e4e90a88eafbaf0a00b88c813ec9e422032c7
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2024-11-06 20:35:07 +0000

    refactor layout logic a bit

--- src/lib/components/CardSkill.svelte
+++ src/lib/components/CardSkill.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
     import type { ClientSideCard } from "$lib/types";
     import { copyCardLink } from "$lib/stores/clipboard";
-    
+
     // TODO: might be nice to tighten this type to Skill
     export let card: ClientSideCard;
 
     const id = card.name.replace(/\s+/g, "_");
 </script>
 
-<div class="p-4 border border-gray-200 rounded-lg shadow-sm" id={id}>
+<div class="p-4 border border-gray-200 rounded-lg shadow-sm" {id}>
     <div class="font-bold text-xl mb-2">
         {card.name}
-        <button
-            onclick={() => copyCardLink(id)}
-            title="Copy link to this item"
-        >
+        <button onclick={() => copyCardLink(id)} title="Copy link to this item">
             🔗
         </button>
     </div>
@@ -37,36 +34,32 @@
     <div
         class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
     >
-        {#each Object.entries(card.tiers) as [tierType, tier]}
-            {#if tierType !== "Legendary"}
-                <div
-                    class={tier.tooltips.length > 0
-                        ? "p-2 rounded-lg bg-gray-100"
-                        : ""}
-                >
-                    {#if tier.tooltips.length > 0}
-                        <div class="font-semibold">
-                            {tierType}
-                        </div>
+        {#each Object.entries(card.tiers).filter(([tierType]) => tierType !== "Legendary") as [tierType, tier]}
+            {#if tier.tooltips.length === 0}
+                <div></div>
+            {:else}
+                <div class={"p-2 rounded-lg bg-gray-100"}>
+                    <div class="font-semibold">
+                        {tierType}
+                    </div>
 
-                        <ul class="ml-4 list-inside list-disc space-y-1">
-                            {#each tier.attributes as attribute}
-                                <li class="text-gray-600">
-                                    <span class="font-medium"
-                                        >{attribute.name}:</span
-                                    >
-                                    {attribute.value}
-                                    {attribute.valueDescriptor}
-                                </li>
-                            {/each}
+                    <ul class="ml-4 list-inside list-disc space-y-1">
+                        {#each tier.attributes as attribute}
+                            <li class="text-gray-600">
+                                <span class="font-medium"
+                                    >{attribute.name}:</span
+                                >
+                                {attribute.value}
+                                {attribute.valueDescriptor}
+                            </li>
+                        {/each}
 
-                            {#each tier.tooltips as tooltip}
-                                <li class="text-gray-600">
-                                    {tooltip}
-                                </li>
-                            {/each}
-                        </ul>
-                    {/if}
+                        {#each tier.tooltips as tooltip}
+                            <li class="text-gray-600">
+                                {tooltip}
+                            </li>
+                        {/each}
+                    </ul>
                 </div>
             {/if}
         {/each}
