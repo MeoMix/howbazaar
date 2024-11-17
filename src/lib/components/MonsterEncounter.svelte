8d4b730b1c28f0a4d831d6ce546aead60b4ba731
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2024-11-17 07:22:21 +0000

    feat: facelift monster card titles

--- src/lib/components/MonsterEncounter.svelte
+++ src/lib/components/MonsterEncounter.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import type { MonsterEncounter as MonsterEncounterType } from "$lib/types";
+    import { Badge } from "flowbite-svelte";
     import MonsterCardItem from "./MonsterCardItem.svelte";
     import MonsterCardSkill from "./MonsterCardSkill.svelte";
 
@@ -8,25 +9,13 @@
 </script>
 
 <div class="mt-8">
-    <div class="font-bold text-3xl mb-1">
+    <div class="font-bold text-3xl mb-2">
         {monsterEncounter.cardName}
     </div>
 
-    <div class="flex mb-1 gap-4">
-        <span class="font-semibold w-24 text-right capitalize whitespace-nowrap"
-            >Health</span
-        >
-        <span class="capitalize">{monsterEncounter.health}</span>
-    </div>
+    <Badge large rounded border color="green">{monsterEncounter.health} health</Badge>
 
-    <div class="flex mb-1 gap-4">
-        <span class="font-semibold w-24 text-right capitalize whitespace-nowrap"
-            >Level</span
-        >
-        <span class="capitalize">{monsterEncounter.level}</span>
-    </div>
-
-    <div class="font-semibold text-xl mt-4 mb-2">Cards</div>
+    <div class="font-semibold text-xl mt-4 mb-2">Items</div>
 
     <div
         class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2"
