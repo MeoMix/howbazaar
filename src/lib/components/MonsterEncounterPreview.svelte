dbe52f7ec94980842a767386afbdf4e25114de05
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2024-11-18 23:23:36 +0000

    lazy load monster encounter preview images

--- src/lib/components/MonsterEncounterPreview.svelte
+++ src/lib/components/MonsterEncounterPreview.svelte
@@ -37,6 +37,7 @@
             class={`absolute top-0 left-0 h-full w-full transition-all scale-110 group-hover:scale-[115%] ${
                 isActive ? "grayscale-[0%]" : "grayscale-[100%]"
             }`}
+            loading="lazy"
         />
         <div
             class="absolute inset-0 bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center"
