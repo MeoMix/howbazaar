b45e668ae2e99917750968ab9f042b9795a598e6
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2024-11-18 21:40:22 +0000

    cut excess routing checks

--- src/routes/monsters/+layout.server.ts
+++ src/routes/monsters/+layout.server.ts
@@ -3,17 +3,12 @@ import parsedMonsters from "$lib/processedMonsters.json" assert { type: "json" }
 import parsedDayHours from "$lib/processedDayHours.json" assert { type: "json" };
 
 import type { ClientSideCard, Monster, MonsterEncounterDay } from "$lib/types";
-import { redirect } from "@sveltejs/kit";
 import { getMonsterEncounterDays } from "$lib/services/monsterEncounterService";
 
 let cards: ClientSideCard[];
 let monsterEncounterDays: MonsterEncounterDay[];
 
-export function load({ url }) {
-    if (url.pathname === '/') {
-        redirect(302, '/items');
-    }
-
+export function load() {
     if (!monsterEncounterDays) {
         if (!cards) {
             cards = parsedCards as ClientSideCard[];
--- src/routes/skills/+layout.server.ts
+++ src/routes/skills/+layout.server.ts
@@ -1,15 +1,9 @@
 import parsedCards from "$lib/processedCards.json" assert { type: "json" };
-
 import type { ClientSideCard } from "$lib/types";
-import { redirect } from "@sveltejs/kit";
 
 let cards: ClientSideCard[];
 
-export function load({ url }) {
-    if (url.pathname === '/') {
-        redirect(302, '/items');
-    }
-
+export function load() {
     if (!cards) {
         cards = parsedCards as ClientSideCard[];
     }
