d2f1f4a304aa127aa885dfc4fe0438a8cf833273
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2024-11-19 22:16:29 +0000

    fix grid cols after removing other tiers

--- src/lib/components/UnifiedTooltips.svelte
+++ src/lib/components/UnifiedTooltips.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div
-    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+    class="grid grid-cols-1"
 >
     <Card size="xl" padding="sm">
         {#each unifiedTooltips as tooltip}
