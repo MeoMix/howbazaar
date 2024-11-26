3cb34eda00efd476e195afe5d9de848ece60d4d0
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2024-11-26 04:46:39 +0000

    fix: change hashes to be generated JIT

--- src/lib/utils/dataUtils.ts
+++ src/lib/utils/dataUtils.ts
@@ -3,27 +3,40 @@ import parsedMonsters from "$lib/processedMonsters.json" assert { type: "json" }
 import parsedDayHours from "$lib/processedDayHours.json" assert { type: "json" };
 import { getMonsterEncounterDays } from "$lib/services/monsterEncounterService";
 import type { ClientSideCard, ClientSideCardItem, ClientSideCardSkill, ClientSideDayHours, Monster, MonsterEncounterDay } from "$lib/types";
-import versionHashes from "$lib/versionHashes";
+import stringify from 'fast-json-stable-stringify';
+import xxhash from 'xxhashjs';
+
+let skillsHash: string | undefined;
+let itemsHash: string | undefined;
+let monsterEncounterDaysHash: string | undefined;
+
+const getHash = (obj: object): string => xxhash.h32(stringify(obj), 0xABCD).toString(16)
 
 export function getVersionedSkills(): { skills: ClientSideCardSkill[]; version: string; } {
     const skills = (parsedCards as ClientSideCard[])
-        .filter((card): card is ClientSideCardSkill => card.type === "Skill")
-        .sort((a, b) => a.name.localeCompare(b.name));
+        .filter((card): card is ClientSideCardSkill => card.type === "Skill");
+
+    if (skillsHash === undefined) {
+        skillsHash = getHash(skills);
+    }
 
     return {
-        skills,
-        version: versionHashes.cardsHash,
+        skills: skills.sort((a, b) => a.name.localeCompare(b.name)),
+        version: skillsHash,
     };
 }
 
 export function getVersionedItems(): { items: ClientSideCardItem[]; version: string; } {
     const items = (parsedCards as ClientSideCard[])
-        .filter((card): card is ClientSideCardItem => card.type === "Item")
-        .sort((a, b) => a.name.localeCompare(b.name));
+        .filter((card): card is ClientSideCardItem => card.type === "Item");
+
+    if (itemsHash === undefined) {
+        itemsHash = getHash(items);
+    }
 
     return {
-        items,
-        version: versionHashes.cardsHash,
+        items: items.sort((a, b) => a.name.localeCompare(b.name)),
+        version: itemsHash,
     };
 }
 
@@ -32,12 +45,14 @@ export function getVersionedMonsterEncounterDays(): { monsterEncounterDays: Mons
     const monsters = parsedMonsters as Monster[];
     const dayHours = parsedDayHours as ClientSideDayHours[];
 
-    const monsterEncounterDays = getMonsterEncounterDays(cards, monsters, dayHours).sort(
-        (dayHourA, dayHourB) => dayHourA.day - dayHourB.day,
-    );
+    const monsterEncounterDays = getMonsterEncounterDays(cards, monsters, dayHours)
+
+    if (monsterEncounterDaysHash === undefined) {
+        monsterEncounterDaysHash = getHash(monsterEncounterDays);
+    }
 
     return {
-        monsterEncounterDays,
-        version: `${versionHashes.cardsHash}-${versionHashes.monstersHash}-${versionHashes.dayHoursHash}`,
+        monsterEncounterDays: monsterEncounterDays.sort((a, b) => a.day - b.day),
+        version: monsterEncounterDaysHash,
     };
 }
