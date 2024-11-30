a10922ab0678eebbb515cfaed166ca78898f8e81
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2024-11-30 05:29:38 +0000

    update contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,89 +2,81 @@
     <title>Contact & Upcoming Features · How Bazaar</title>
 </svelte:head>
 
-<div class="bold text-4xl mb-8 mt-8">Hi!</div>
+<div class="font-semibold text-4xl my-4">Hi!</div>
 
-<div class="text-lg mb-8">
-    <div class="my-2">
+<ul class="text-lg mb-8 leading-loose">
+    <li>
         Thanks for visiting my humble little website :) I hope you're finding it
         useful so far.
-    </div>
-    <div class="my-2">
+    </li>
+    <li>
         I'm very interested in making this the best spot to get all your The
         Bazaar information.
-    </div>
-    <div class="my-2">
+    </li>
+    <li>
         I would really appreciate it if you'd contact me with any ideas, feature
         requests, or bug reports!
-    </div>
-    <div class="my-2">
-        I can be reached on Discord, <span class="font-bold">@MeoMix</span> on
-        The Bazaar's server or User ID: 303241372989915136, or on Reddit
-        <a class="bold" href="https://www.reddit.com/user/MeoMix/"
+    </li>
+    <li>
+        I would prefer if you reached out via the <a
+            class="font-bold text-blue-600"
+            href="https://discord.gg/VrGGFYEXXz"
+            target="_blank">HowBazaar Discord Server</a
+        >, but you can also reach me directly via
+        <span class="font-bold">@MeoMix</span>
+        on The Bazaar's server (or via User ID: 303241372989915136), or on Reddit
+        <a
+            class="font-bold text-blue-600"
+            href="https://www.reddit.com/user/MeoMix/"
             >https://www.reddit.com/user/MeoMix/</a
         >
-    </div>
-    <div class="my-2">
+    </li>
+    <li>
         I'm terminally online, so you should have no trouble reaching me, but I
         do sleep from 10pm to 6am PST. Feel free to leave me a message if I'm
         not responding.
-    </div>
-    <div class="my-2">
+    </li>
+    <li>
         Oh, and P.S. I have spent <span class="font-bold text-red-500"
             >~200+ hours</span
         >
         building this site, working 10 hours/day since launch.
         <span class="font-bold text-green-500">Please be kind</span> when expressing
         constructive criticism about the website. Thank you.
-    </div>
-</div>
+    </li>
+</ul>
 
 <div class="text-lg mb-8">
-    <div class="mb-4 font-bold">
-        Upcoming Features as of November 21th, 2024:
+    <div class="text-2xl mb-4 font-semibold">
+        Upcoming Features as of November 29th, 2024:
     </div>
 
     <div class="mb-8">
         <span class="font-semibold">Soon (tomorrow?)</span>
 
-        <ul class="ml-8">
+        <ul class="ml-8 leading-loose">
             <li>
-                <span class="line-through">
-                    Optimize website bandwidth to survive increasing user load
-                    without incurring large server costs. Thank you so much for
-                    all the interest and support!
+                <span class="">
+                    Add monsters to the Monsters page which are only able to be
+                    encountered via events rather than Hour 3 encounters.
                 </span>
-                <span class="font-bold text-green-500">Done!</span>
+                <span class="font-bold text-green-500"></span>
             </li>
             <li>
-                <span class="line-through">
-                    Polish some lingering issues with the design around
-                    legibility, colors, lightmode.
+                <span class="">
+                    Broadly improve the UX of the Monsters Page. Add a search
+                    bar, try to scale images down a bit, make it easier to tell
+                    items/skills at a glance.
                 </span>
-                <span class="font-bold text-green-500"
-                    >Mostly Done! I'll work on the UI more in a week, need to
-                    focus on features.</span
-                >
+                <span class="font-bold text-green-500"></span>
             </li>
             <li>
-                <span class="line-through">
-                    Add support for images to Items and Skills pages! This has
-                    been a long-standing request.
+                <span class="">
+                    Confirm that new image caching strategy is working well and
+                    improves load time of website / reduces network requests to
+                    server.
                 </span>
-                <span class="font-bold text-green-500"
-                    >Mostly Done! I'm not 100% sold on the UI for presenting
-                    images, but they're there!</span
-                >
-            </li>
-            <li>
-                <span class="line-through">
-                    Assuming a Wednesday patch comes out, update website to
-                    reflect new patch changes.
-                </span>
-
-                <span class="font-bold text-green-500">
-                    Patch didn't change any data available to my scripts.
-                </span>
+                <span class="font-bold text-green-500"></span>
             </li>
         </ul>
     </div>
@@ -92,26 +84,32 @@
     <div class="mb-8">
         <span class="font-semibold">A little later (this week/weekend?)</span>
 
-        <ul class="ml-8">
+        <ul class="ml-8 leading-loose">
             <li>
-                Rewrite some server code related to parsing enchantment data. A
-                couple of enchantments are hardcoded currently due to
-                limitations in my scripts. I do not want to be in a position
-                where I have to manually manage new card/enchantment additions
-                to the game.
-            </li>
-
-            <li>
-                <span class="line-through">
-                    Add the ability to use Item/Skill search to find
-                    Items/Skills which are dropped by specific Monster
-                    Encounters.
+                <span class="">
+                    Misc. Bug Fixes - 2x Blue Piggles R are showing, de-dupe
+                    "Monster Encounter" links shown on items, searching for
+                    "Barrel" causes "Double Barrel" to disappear, Runic Greataxe
+                    says "Lifesteal 100" when just "Lifesteal" is sufficient,
+                    enchanted monster items don't show tags introduced via
+                    enchantment (i.e. Toxic Fang should be Poison)
                 </span>
-                <span class="font-bold text-green-500"> Done! </span>
+                <span class="font-bold text-green-500"></span>
             </li>
             <li>
-                Add monsters to the Monsters page which are only able to be
-                encountered via events rather than Hour 3 encounters.
+                <span class="">
+                    Try to rework navigation layout to allow for Search to move
+                    to the Sticky Header so it's always accessible even if user
+                    is scrolled down page.
+                </span>
+                <span class="font-bold text-green-500"></span>
+            </li>
+            <li>
+                <span class="">
+                    Improve SEO support so users can more easily find website
+                    via Google
+                </span>
+                <span class="font-bold text-green-500"></span>
             </li>
         </ul>
     </div>
@@ -119,12 +117,11 @@
     <div class="mb-8">
         <span class="font-semibold"> After that... (early next week)</span>
 
-        <ul class="ml-8">
+        <ul class="ml-8 leading-loose">
             <li>
                 Begin creating a new page to display Vendor information. Allow
-                users to see what items/skills they could get from a vendor with
-                bronze/silver/gold/diamond tier probabilities influenced by
-                filtering on current day.
+                users to see what items/skills they could get from various
+                vendors. Include % chance to get various tiers.
             </li>
         </ul>
     </div>
@@ -152,8 +149,8 @@
 <div class="text-lg">
     <div>Okie. That's all~~</div>
 
-    <div class="text-red-500">
+    <div class="font-bold text-red-500">
         {"<3"}
     </div>
-    <div>- Meo</div>
+    <div>~ Meo</div>
 </div>
