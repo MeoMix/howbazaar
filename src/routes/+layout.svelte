de703e49ea0ba4cb2d4f3b923c90cf992c98aea8
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2024-12-04 03:47:45 +0000

    disable attempting to load analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import "../app.css";
-    import { dev } from "$app/environment";
-    import { inject } from "@vercel/analytics";
-    inject({ mode: dev ? "development" : "production" });
+    // import { dev } from "$app/environment";
+    // import { inject } from "@vercel/analytics";
+    // inject({ mode: dev ? "development" : "production" });
 
     import {
         Toast,
