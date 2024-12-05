8d53db17e042b24271efe0f0bf1494caf3853d2c
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2024-12-05 21:43:09 +0000

    feat: adopt goat-counter instead of using vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
     import "../app.css";
-    // import { dev } from "$app/environment";
-    // import { inject } from "@vercel/analytics";
-    // inject({ mode: dev ? "development" : "production" });
 
     import {
         Toast,
