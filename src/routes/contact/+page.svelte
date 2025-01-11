a23a53a242682408f06d97e980bebac294a19a83
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2025-01-11 06:50:16 +0000

    Update about

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    import Divider from "$lib/components/Divider.svelte";
+
+</script>
 <svelte:head>
     <title>Contact & Upcoming Features · How Bazaar</title>
 </svelte:head>
@@ -40,7 +44,7 @@
         Oh, and P.S. I have spent <span class="font-bold text-red-500"
             >~200+ hours</span
         >
-        building this site, working 10 hours/day since launch.
+        building this site.
         <span class="font-bold text-green-500">Please be kind</span> when expressing
         constructive criticism about the website. Thank you.
     </li>
@@ -48,101 +52,64 @@
 
 <div class="text-lg mb-8">
     <div class="text-2xl mb-4 font-semibold">
-        Upcoming Features as of Dec 5th, 2024:
+        Upcoming Features as of Jan 10th, 2024:
     </div>
+    
+    <div>
+        <div class="mb-8">
+            <span class="font-semibold">Soon</span>
+    
+            <ul class="ml-8 leading-loose">
+                <li>
+                    <span>
+                        Fix some UX issues with search and exact text match. i.e. search for Barrel doesn't show Double Barrel.
+                    </span>
+                    <span class="font-bold text-green-500"></span>
+                </li>
+                <li>
+                    <span>
+                        Play around with making search sticky so you don't have to scroll up each time to change search.
+                    </span>
+                    <span class="font-bold text-green-500"></span>
+                </li>
+                <li>
+                    <span>
+                        Small improvements to Monsters item data - be able to see multi-tier preview via tooltip, etc.
+                    </span>
+                    <span class="font-bold text-green-500"></span>
+                </li>
+            </ul>
+        </div>
 
-    <div class="mb-8">
-        <span class="font-semibold">Soon (tomorrow?)</span>
+        <div class="mb-8">
+            <span class="font-semibold">A little later</span>
+    
+            <ul class="ml-8 leading-loose">
+                <li>
+                    <span>
+                        Improve SEO accessibility
+                    </span>
+                    <span class="font-bold text-green-500"></span>
+                </li>
+                <li>
+                    <span>
+                        Add ability to donate to support website. Consider other options for offsetting server costs.
+                    </span>
+                    <span class="font-bold text-green-500"></span>
+                </li>
+                <li>
+                    <span>
+                        Begin creating mod. Similar offerings to Bazaar Tracker, but without ads and overwolf.
+                        This will also allow website to crowdsource information about the game that isn't able to be scripted easily such as data regarding PvE, Vendors, etc.
+                        Hopefully this will be good enough to get people to switch over even though it's late to the game. :)
+                    </span>
+                    <span class="font-bold text-green-500"></span>
+                </li>
+            </ul>
+        </div>
+    
+    
 
-        <ul class="ml-8 leading-loose">
-            <li>
-                <span class="line-through">
-                    Add monsters to the Monsters page which are only able to be
-                    encountered via events rather than Hour 3 encounters.
-                </span>
-                <span class="font-bold text-green-500">Done!</span>
-            </li>
-            <li>
-                <span class="line-through">
-                    Broadly improve the UX of the Monsters Page. Add a search
-                    bar, try to scale images down a bit, make it easier to tell
-                    items/skills at a glance.
-                </span>
-                <span class="font-bold text-green-500">Done!</span>
-            </li>
-            <li>
-                <span class="">
-                    Confirm that new image caching strategy is working well and
-                    improves load time of website / reduces network requests to
-                    server.
-                </span>
-                <span class="font-bold text-green-500">This change was reverted and I'm investigating why it didn't work as I'd hoped</span>
-            </li>
-        </ul>
-    </div>
-
-    <div class="mb-8">
-        <span class="font-semibold">A little later (this week/weekend?)</span>
-
-        <ul class="ml-8 leading-loose">
-            <li>
-                <span class="">
-                    Misc. Bug Fixes - 2x Blue Piggles R are showing, de-dupe
-                    "Monster Encounter" links shown on items, searching for
-                    "Barrel" causes "Double Barrel" to disappear, Runic Greataxe
-                    says "Lifesteal 100" when just "Lifesteal" is sufficient,
-                    enchanted monster items don't show tags introduced via
-                    enchantment (i.e. Toxic Fang should be Poison)
-                </span>
-                <span class="font-bold text-green-500"></span>
-            </li>
-            <li>
-                <span class="">
-                    Try to rework navigation layout to allow for Search to move
-                    to the Sticky Header so it's always accessible even if user
-                    is scrolled down page.
-                </span>
-                <span class="font-bold text-green-500"></span>
-            </li>
-            <li>
-                <span class="">
-                    Improve SEO support so users can more easily find website
-                    via Google
-                </span>
-                <span class="font-bold text-green-500"></span>
-            </li>
-        </ul>
-    </div>
-
-    <div class="mb-8">
-        <span class="font-semibold"> After that... (early next week)</span>
-
-        <ul class="ml-8 leading-loose">
-            <li>
-                Begin creating a new page to display Vendor information. Allow
-                users to see what items/skills they could get from various
-                vendors. Include % chance to get various tiers.
-            </li>
-        </ul>
-    </div>
-
-    <div class="mb-8">
-        <span class="font-semibold">Much later</span>
-        <ul class="ml-8">
-            <li>
-                Allow users to prototype builds online. Users will be able to
-                share a link to their build to discuss it with others. This will
-                be rudimentary. It won't show DPS or anything like that. It'll
-                just be a way to select up to 10 items, some enchantments, some
-                skills, and then share those selections with others.
-            </li>
-            <li>
-                Create an in-game extension to allow users to gain insights from
-                the data exposed by the website. Allow users to submit their
-                in-game data to the website to allow the website to show popular
-                items/builds.
-            </li>
-        </ul>
     </div>
 </div>
 
