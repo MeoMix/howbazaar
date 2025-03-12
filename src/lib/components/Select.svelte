4d3f496947c5f4ef49b50a402f9d18c7f6ee817e
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2025-03-12 02:28:06 +0000

    Fix select text alignment when full width is being used

--- src/lib/components/Select.svelte
+++ src/lib/components/Select.svelte
@@ -20,7 +20,7 @@
 <div class="relative w-full">
     <Button
         size="sm"
-        class="py-1 px-2 w-full text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-bazaar-orange focus:border-bazaar-orange dark:bg-bazaar-brown dark:border-bazaar-brown600 dark:placeholder-gray-400 dark:text-bazaar-tan700 dark:focus:ring-bazaar-orange dark:focus:border-bazaar-orange"
+        class="flex justify-between text-nowrap py-1 px-2 w-full text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-bazaar-orange focus:border-bazaar-orange dark:bg-bazaar-brown dark:border-bazaar-brown600 dark:placeholder-gray-400 dark:text-bazaar-tan700 dark:focus:ring-bazaar-orange dark:focus:border-bazaar-orange"
     >
         {options.find((option) => option.value === selectedOption)?.name}
         <ChevronDownOutline
