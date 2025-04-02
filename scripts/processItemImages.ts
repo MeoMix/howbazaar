08ca169ac518c981cf4f752281727a585dc394f1
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2025-04-02 21:02:55 +0000

    fix: renamed items

--- scripts/processItemImages.ts
+++ scripts/processItemImages.ts
@@ -122,6 +122,9 @@ const cleanFileName = (fileName: string): string => {
     'DarkwaterAnglerfish (1)': 'DarkwaterAnglerfish',
     'Nanobots': 'Nanobot',
     'SustainingPotion': 'NoxiousPotion', // It's weird because the item is definitely Noxious Potion, and there's an image for it, but it's not the right one.
+    'FossilizedFemur': 'MagnusFemur',
+    'PrimordialDepthCharge': 'ElementalDepthCharge',
+    'FuelRod': 'DefenseMatrix'
   };
 
   // Sometimes there's a literal space at the end of the filename. Madness.
