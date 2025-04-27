aec2ae64811970453814ed43676cfdd242205c4c
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2025-04-27 02:42:39 +0000

    fix grid cols

--- src/lib/components/MerchantCard.svelte
+++ src/lib/components/MerchantCard.svelte
@@ -43,7 +43,7 @@
             items={merchantItems}
             {listItem}
             listItemName="item"
-            listClasses="grid gap-2 grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 col-span-full md:col-span-1"
+            listClasses="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 col-span-full md:col-span-1"
         />
     </div>
 </div>
