ed9ff089519509ace5c91dd2159e82095466f2eb
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2025-05-08 00:17:13 +0000

    fix: ensure Regen is colored green

--- src/lib/utils/keywordUtil.ts
+++ src/lib/utils/keywordUtil.ts
@@ -6,7 +6,7 @@ export type KeywordPart = {
 
 const keywordEffects: Record<string, string[]> = {
     damage: ["Crit Chance", "Damage", "Damages"],
-    heal: ["Heal", "Heals", "Health", "Max Health", "Regeneration"],
+    heal: ["Heal", "Heals", "Health", "Max Health", "Regeneration", "Regen"],
     ammo: ["Ammo", "Max Ammo"],
     shield: ["Shield", "Shields"],
     haste: ["Haste", "Hastes"],
